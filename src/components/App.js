69c86607ad90fb73faf5981b36e0ca5f7860fb7f
Atul Kumar Singh <136229949+uniqueatul@users.noreply.github.com>
2025-07-16 12:44:15 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
-
+import {useState} from 'react'
 import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [count, setCount] = useState(0)
+
+  const Handler = () =>{
+    setCount (count + 1)
+    
+  }
+  
   return (
     <div>
         {/* Do not remove the main div */}
+
+    <p>
+
+      Button was clicked {count} time 
+
+    </p>
+    <button onClick={Handler}>
+      clicked me
+    </button>
+      
     </div>
   )
 }
